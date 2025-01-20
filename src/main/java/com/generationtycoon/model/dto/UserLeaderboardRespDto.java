e5a719effd3a2b8de9f7a0b68fe5f1ec99acbba3
noemi <noemi.battezzato@gmail.com>
2025-01-20 15:27:50 +0100

    Aggiunti builder ai dto user

--- src/main/java/com/generationtycoon/model/dto/UserLeaderboardRespDto.java
+++ src/main/java/com/generationtycoon/model/dto/UserLeaderboardRespDto.java
@@ -1,7 +1,48 @@
 package com.generationtycoon.model.dto;
 
 import com.generationtycoon.model.entities.Difficulty;
+import com.generationtycoon.model.entities.User;
 
 public record UserLeaderboardRespDto(String username, Difficulty difficulty, double score) {
+
+
+	public static UserLeaderboardRespDtoBuilder of()
+	{
+		return new UserLeaderboardRespDtoBuilder();
+	}
+
+	public static class UserLeaderboardRespDtoBuilder
+	{
+		private String username;
+		private Difficulty difficulty;
+		private double score;
+
+		private UserLeaderboardRespDtoBuilder(){}
+
+		public UserLeaderboardRespDtoBuilder username(String username)
+		{
+			this.username = username;
+			return this;
+		}
+
+		public UserLeaderboardRespDtoBuilder difficulty(Difficulty difficulty)
+		{
+			this.difficulty = difficulty;
+			return this;
+		}
+		public UserLeaderboardRespDtoBuilder score(double score)
+		{
+			this.score = score;
+			return this;
+		}
+
+		public UserLeaderboardRespDto build()
+		{
+			return new UserLeaderboardRespDto(username, difficulty, score);
+		}
+
+
+	}
+
 
 }
