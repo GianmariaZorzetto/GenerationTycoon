2e64399217e2ba78424cdb4e3d12629a1346f987
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-01-20 17:51:07 +0100

    aggiunta documentazione

--- src/main/java/com/generationtycoon/model/repositories/KaboomRepository.java
+++ src/main/java/com/generationtycoon/model/repositories/KaboomRepository.java
@@ -3,6 +3,8 @@ package com.generationtycoon.model.repositories;
 import com.generationtycoon.model.entities.Kaboom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KaboomRepository extends JpaRepository<Kaboom, Long>
-{
+/**
+ * Repository di {@link Kaboom}
+ */
+public interface KaboomRepository extends JpaRepository<Kaboom, Long> {
 }
