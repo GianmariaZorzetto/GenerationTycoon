e2a58930db685b15bffa51ee321d20241b9bfc76
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-01-20 15:17:31 +0100

    aggiunto builder UserRegistrationReqDtoBuilder

--- src/main/java/com/generationtycoon/model/dto/UserRegistrationReqDto.java
+++ src/main/java/com/generationtycoon/model/dto/UserRegistrationReqDto.java
@@ -2,6 +2,44 @@ package com.generationtycoon.model.dto;
 
 import com.generationtycoon.model.entities.Difficulty;
 
-public record UserRegistrationReqDto(String email, String username, String password, Difficulty difficulty)
-{
+public record UserRegistrationReqDto(String email, String username, String password,
+                                     Difficulty difficulty) {
+
+    public static UserRegistrationReqDtoBuilder of() {
+        return new UserRegistrationReqDtoBuilder();
+    }
+
+    public static class UserRegistrationReqDtoBuilder {
+        private String email;
+        private String username;
+        private String password;
+        private Difficulty difficulty;
+
+        private UserRegistrationReqDtoBuilder() {
+        }
+
+        public UserRegistrationReqDtoBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserRegistrationReqDtoBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserRegistrationReqDtoBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserRegistrationReqDtoBuilder difficulty(Difficulty difficulty) {
+            this.difficulty = difficulty;
+            return this;
+        }
+
+        public UserRegistrationReqDto build() {
+            return new UserRegistrationReqDto(email, username, password, difficulty);
+        }
+    }
 }
