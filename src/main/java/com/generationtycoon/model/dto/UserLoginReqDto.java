446e78455acd8e9450cb4ee2024bf36f6a90e679
noemi <noemi.battezzato@gmail.com>
2025-01-20 15:27:50 +0100

    Aggiunti builder ai dto user

--- src/main/java/com/generationtycoon/model/dto/UserLoginReqDto.java
+++ src/main/java/com/generationtycoon/model/dto/UserLoginReqDto.java
@@ -2,4 +2,38 @@ package com.generationtycoon.model.dto;
 
 public record UserLoginReqDto(String email, String password)
 {
+
+	public static UserLoginReqDtoBuilder of()
+	{
+		return new UserLoginReqDtoBuilder();
+	}
+
+	public static class UserLoginReqDtoBuilder
+	{
+		private String email;
+		private String password;
+
+		private UserLoginReqDtoBuilder()
+		{
+		}
+
+		public UserLoginReqDtoBuilder email(String email)
+		{
+			this.email = email;
+			return this;
+		}
+
+		public UserLoginReqDtoBuilder password(String password)
+		{
+			this.password = password;
+			return this;
+		}
+
+		public UserLoginReqDto build()
+		{
+			return new UserLoginReqDto(email, password);
+		}
+
+	}
+
 }
