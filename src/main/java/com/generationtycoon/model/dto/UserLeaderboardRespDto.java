5d1d7c2627c95440260f2aabd08be15c30aab7ce
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-01-20 15:17:31 +0100

    aggiunto builder UserRegistrationReqDtoBuilder

--- src/main/java/com/generationtycoon/model/dto/UserLeaderboardRespDto.java
+++ src/main/java/com/generationtycoon/model/dto/UserLeaderboardRespDto.java
@@ -2,6 +2,6 @@ package com.generationtycoon.model.dto;
 
 import com.generationtycoon.model.entities.Difficulty;
 
-public record UserLeaderboardRespDto(String username, Difficulty difficulty, double score)
-{
+public record UserLeaderboardRespDto(String username, Difficulty difficulty, double score) {
+
 }
