5ac8035ea4030229b90e955aecd9cd72ffdf10c1
noemi <noemi.battezzato@gmail.com>
2025-01-21 13:02:01 +0100

    Aggiunta firma metodi ControllerHelper

--- src/main/java/com/generationtycoon/controllers/helpers/ControllerHelper.java
+++ src/main/java/com/generationtycoon/controllers/helpers/ControllerHelper.java
@@ -1,8 +1,27 @@
 package com.generationtycoon.controllers.helpers;
 
+import com.generationtycoon.model.dto.*;
+import com.generationtycoon.model.entities.Difficulty;
+
+import java.util.List;
+
 /**
  * Interfaccia che definisce i metodi di un controller helper.
  */
 public interface ControllerHelper {
     // TODO implementare i metodi non appena abbiamo fatto i DTO
+
+	List<KaboomRespDto> getAllKabooms();
+
+	KaboomRespDto getKaboomById(long id);
+
+	List<UserLeaderboardRespDto> getAllUsersOnTheLeaderboard();
+
+	UserLoginRespDto getUserById(long id);
+
+	List<UserLeaderboardRespDto> getUsersByDifficulty(Difficulty difficulty);
+
+	UserLeaderboardRespDto modifyUserInLeaderboard(long id, UserLeaderboardReqDto dtoUser);
+
+	void deleteUser(long id);
 }
