fbb4d4b9d3ca10b5624a0f158b6f1239d7952317
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-01-21 11:43:08 +0100

    aggiunta logica di creazione del token di comunicazione tra front e back end

--- src/main/java/com/generationtycoon/utils/credentials/CredentialService.java
+++ src/main/java/com/generationtycoon/utils/credentials/CredentialService.java
@@ -2,18 +2,24 @@ package com.generationtycoon.utils.credentials;
 
 import com.generationtycoon.controllers.exceptions.InvalidEmailException;
 import com.generationtycoon.controllers.exceptions.InvalidPasswordException;
+import com.generationtycoon.controllers.exceptions.InvalidTokenException;
+import com.generationtycoon.controllers.exceptions.UserMissingException;
 import com.generationtycoon.model.dto.UserLoginReqDto;
 import com.generationtycoon.model.dto.UserLoginRespDto;
 import com.generationtycoon.model.dto.UserRegistrationReqDto;
 import com.generationtycoon.model.entities.User;
 import com.generationtycoon.model.repositories.UserRepository;
+import com.generationtycoon.utils.validator.Validator;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
+import java.util.StringJoiner;
 
 @Service
 public class CredentialService {
@@ -21,6 +27,26 @@ public class CredentialService {
 
     public CredentialService(@Autowired UserRepository userRepo) {
         this.userRepo = userRepo;
+    }
+
+    private String generateToken(String email) {
+        StringJoiner sj = new StringJoiner("-");
+        int offset = new Random().nextInt(20);
+        sj.add(String.valueOf(offset));
+        for (char c : email.toCharArray())
+            sj.add(String.valueOf(c + offset));
+        return sj.toString();
+    }
+
+    private String convertToken(String token) {
+        String[] parts = token.split("-");
+        StringBuilder t = new StringBuilder();
+        int offset = Integer.parseInt(parts[0]);
+        Arrays.stream(parts).skip(1).forEach(part -> {
+            int byteInt = Integer.parseInt(part) - offset;
+            t.append((char) byteInt);
+        });
+        return t.toString();
     }
 
     public UserLoginRespDto login(UserLoginReqDto dto)
@@ -31,12 +57,15 @@ public class CredentialService {
         String passwordHashed = DigestUtils.md5Hex(dto.password());
         if (!user.getPassword().equals(passwordHashed))
             throw new InvalidPasswordException("Password non corretta.");
-        // TODO creazione di un token.
-        return new UserLoginRespDto(user.getEmail());
+        return new UserLoginRespDto(generateToken(user.getEmail()));
     }
 
-    public User register(UserRegistrationReqDto dto) throws NullPointerException, IllegalArgumentException {
-        String passwordHashed = DigestUtils.md5Hex(Objects.requireNonNull(dto, "Dto null").password());
+    public User register(UserRegistrationReqDto dto)
+            throws NullPointerException, InvalidPasswordException, IllegalArgumentException {
+        String psw = Objects.requireNonNull(dto, "Dto null").password();
+        if (!Validator.PASSWORD.validate(psw))
+            throw new InvalidPasswordException("Password non corretta.");
+        String passwordHashed = DigestUtils.md5Hex(psw);
         return userRepo.save(
                 User.builder()
                         .email(dto.email())
@@ -48,14 +77,15 @@ public class CredentialService {
         );
     }
 
-    public User getUserByToken() {
-        // TODO fare logica non appena fatta logica creazione di un token.
+    public User getUserByToken() throws NullPointerException, InvalidTokenException, UserMissingException {
         ServletRequestAttributes req = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        String token =
+        String tokenReq =
                 Objects.requireNonNull(req, "Errore, request null.")
                         .getRequest()
                         .getHeader("token");
-
-        return null;
+        if (tokenReq == null)
+            throw new InvalidTokenException("Token non trovato.");
+        String token = convertToken(tokenReq);
+        return userRepo.findByEmail(token).orElseThrow(() -> new UserMissingException("User non trovato"));
     }
 }
