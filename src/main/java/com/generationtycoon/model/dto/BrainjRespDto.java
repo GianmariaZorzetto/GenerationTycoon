410088948d14b9f2b70ec589062c2fefb5987912
GianmariaZorzetto <gianzorzi2001@gmail.com>
2025-01-22 11:44:10 +0100

    aggiunto builder a brainj

--- src/main/java/com/generationtycoon/model/dto/BrainjRespDto.java
+++ src/main/java/com/generationtycoon/model/dto/BrainjRespDto.java
@@ -1,42 +1,35 @@
 package com.generationtycoon.model.dto;
 
-public record BrainjRespDto(String question, String answer)
-{
+public record BrainjRespDto(String question, String answer) {
 
-	public static BrainjRespDtoBuilder builder()
-	{
-		return new BrainjRespDtoBuilder();
-	}
+    public static BrainjRespDtoBuilder builder() {
+        return new BrainjRespDtoBuilder();
+    }
 
-	public static class BrainjRespDtoBuilder
-	{
+    public static class BrainjRespDtoBuilder {
 
-		private String question;
+        private String question;
 
-		private String answer;
+        private String answer;
 
 
-		private BrainjRespDtoBuilder()
-		{
-		}
+        private BrainjRespDtoBuilder() {
+        }
 
-		public BrainjRespDtoBuilder question(String question)
-		{
-			this.question = question;
-			return this;
-		}
+        public BrainjRespDtoBuilder question(String question) {
+            this.question = question;
+            return this;
+        }
 
-		public BrainjRespDtoBuilder answer(String answer)
-		{
-			this.answer = answer;
-			return this;
-		}
+        public BrainjRespDtoBuilder answer(String answer) {
+            this.answer = answer;
+            return this;
+        }
 
-		public BrainjRespDto build()
-		{
-			return new BrainjRespDto(question, answer);
-		}
+        public BrainjRespDto build() {
+            return new BrainjRespDto(question, answer);
+        }
 
-	}
+    }
 
 }
--- src/main/java/com/generationtycoon/model/entities/Brainj.java
+++ src/main/java/com/generationtycoon/model/entities/Brainj.java
@@ -3,32 +3,89 @@ package com.generationtycoon.model.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
+import java.util.Objects;
+
 @Entity
-public class Brainj extends BaseEntity
-{
-	@Column(nullable = false)
-	private String question;
+public class Brainj extends BaseEntity {
+    @Column(nullable = false)
+    private String question;
 
-	@Column(nullable = false)
-	private String answer;
+    @Column(nullable = false)
+    private String answer;
 
-	public String getQuestion()
-	{
-		return question;
-	}
+    public Brainj() {
+    }
 
-	public void setQuestion(String question)
-	{
-		this.question = question;
-	}
+    private Brainj(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
 
-	public String getAnswer()
-	{
-		return answer;
-	}
+    private Brainj(Long id, String question, String answer) throws IllegalArgumentException, NullPointerException {
+        super(id);
+        this.question = question;
+        this.answer = answer;
+    }
 
-	public void setAnswer(String answer)
-	{
-		this.answer = answer;
-	}
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) throws NullPointerException, IllegalArgumentException {
+        if (Objects.requireNonNull(question, "Domanda null.").isBlank())
+            throw new IllegalArgumentException("Domanda vuoto.");
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) throws NullPointerException, IllegalArgumentException {
+        if (Objects.requireNonNull(answer, "Risposta null.").isBlank())
+            throw new IllegalArgumentException("Risposta vuota.");
+        this.answer = answer;
+    }
+
+    public static BrainjBuilder builder() {
+        return new BrainjBuilder();
+    }
+
+    public static class BrainjBuilder {
+        private String question;
+        private String answer;
+        private Long id;
+
+        private BrainjBuilder() {
+            question = null;
+            answer = null;
+            id = null;
+        }
+
+        public BrainjBuilder question(String question) {
+            if (Objects.requireNonNull(question, "Domanda null.").isBlank())
+                throw new IllegalArgumentException("Domanda vuota.");
+            this.question = question;
+            return this;
+        }
+
+        public BrainjBuilder answer(String answer) {
+            if (Objects.requireNonNull(answer, "Risposta null.").isBlank())
+                throw new IllegalArgumentException("Risposta vuota.");
+            this.answer = answer;
+            return this;
+        }
+
+        public BrainjBuilder id(Long id) {
+            if (Objects.requireNonNull(id, "Id null.") < 1)
+                throw new IllegalArgumentException("Id vuota.");
+            this.id = id;
+            return this;
+        }
+
+        public Brainj build() {
+            if (id == null) return new Brainj(question, answer);
+            return new Brainj(id, question, answer);
+        }
+    }
 }
