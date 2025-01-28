43da7a5ccc8d32cbec05e8a1ece8e2188eba3a47
Riccardo <panettariccardo97@gmail.com>
2025-01-28 11:45:52 +0100

    Java doc BrainjController e KaboomController e modifica ScoreLogic (java doc UserController da completare)

--- src/main/java/com/generationtycoon/controllers/KaboomController.java
+++ src/main/java/com/generationtycoon/controllers/KaboomController.java
@@ -1,6 +1,7 @@
 package com.generationtycoon.controllers;
 
 import com.generationtycoon.controllers.helpers.ControllerHelper;
+import com.generationtycoon.model.dto.BrainjRespDto;
 import com.generationtycoon.model.dto.KaboomRespDto;
 import com.generationtycoon.utils.credentials.CredentialService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Controller REST per la gestione delle risorse "Kaboom".
+ * Offre endpoint per ottenere tutti i Kaboom o un Kaboom specifico tramite il suo ID.
+ */
 @RestController
 @RequestMapping("api/kabooms")
 public class KaboomController {
@@ -18,18 +23,38 @@ public class KaboomController {
     private final ControllerHelper ch;
     private final CredentialService service;
 
+    /**
+     * Costruttore per l'iniezione delle dipendenze.
+     *
+     * @param ch      il {@link ControllerHelper} per eseguire le operazioni logiche.
+     * @param service il {@link CredentialService} per la gestione delle credenziali.
+     */
     @Autowired
     public KaboomController(ControllerHelper ch, CredentialService service) {
         this.ch = ch;
         this.service = service;
     }
 
+    /**
+     * Endpoint per ottenere la lista di tutti i quiz di tipo "kaboom".
+     * Questo metodo verifica prima il token dell'utente tramite il {@link CredentialService}.
+     *
+     * @return una lista di {@link KaboomRespDto} che rappresenta tutti i quiz disponibili.
+     */
     @GetMapping
     public List<KaboomRespDto> getAll() {
         service.getUserByToken();
         return ch.getAllKabooms();
     }
 
+    /**
+     * Endpoint per ottenere un quiz di tipo "kaboom" specifico dato il suo ID.
+     * Questo metodo verifica prima il token dell'utente tramite il {@link CredentialService}.
+     *
+     * @param id l'ID del quiz da cercare.
+     * @return un {@link KaboomRespDto} che rappresenta il quiz corrispondente all'ID fornito.
+     * @throws com.generationtycoon.controllers.exceptions.KaboomMissingException se il quiz con l'ID specificato non esiste.
+     */
     @GetMapping("/{id}")
     public KaboomRespDto getOne(@PathVariable Long id) {
         service.getUserByToken();
--- src/main/java/com/generationtycoon/controllers/UserController.java
+++ src/main/java/com/generationtycoon/controllers/UserController.java
@@ -13,6 +13,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller per la gestione delle operazioni relative agli utenti nel sistema.
+ * <p>
+ * Questo controller si occupa delle operazioni di login, registrazione, aggiornamento e cancellazione degli utenti.
+ * Inoltre, fornisce metodi per recuperare i punteggi degli utenti e altre informazioni relative.
+ */
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -21,6 +27,14 @@ public class UserController {
     private final ScoreLogic sl;
     private final DtoConverter converter;
 
+    /**
+     * Costruttore per l'iniezione delle dipendenze.
+     *
+     * @param ch      il {@link ControllerHelper} per eseguire le operazioni logiche.
+     * @param cs il {@link CredentialService} per la gestione delle credenziali.
+     * @param sl      lo {@link ScoreLogic} per calcolare e gestire i punteggi degli utenti.
+     * @param converter il {@link DtoConverter} per la conversione dei DTO tra il modello di dominio e le risposte del controller.
+     */
     @Autowired
     public UserController(ControllerHelper ch,
                           CredentialService cs,
@@ -32,6 +46,24 @@ public class UserController {
         this.converter = converter;
     }
 
+
+    /**
+     * Endpoint per la registrazione di un nuovo utente.
+     * <p>
+     * Questo metodo gestisce la registrazione di un utente attraverso i dati forniti dal client.
+     * Una volta registrato l'utente, il metodo genera automaticamente una richiesta di login per autenticare l'utente
+     * appena registrato e restituisce un token di accesso.
+     *
+     * @param dto il {@link UserRegistrationReqDto} contenente i dati per la registrazione dell'utente,
+     *            inclusi l'email, il nome utente, la password e la difficoltà selezionata.
+     *
+     * @return un oggetto {@link UserLoginRespDto} contenente il token di accesso dell'utente,
+     *         il suo ID, il nome utente, il punteggio e la difficoltà selezionata.
+     *
+     * @throws NullPointerException se {@code dto} è {@code null}.
+     * @throws com.generationtycoon.controllers.exceptions.InvalidEmailException se l'email fornita non è valida.
+     * @throws com.generationtycoon.controllers.exceptions.InvalidPasswordException se la password non soddisfa i criteri di validità.
+     */
     @PostMapping("/register")
     public UserLoginRespDto register(@RequestBody UserRegistrationReqDto dto) {
         cs.register(dto);
@@ -40,12 +72,36 @@ public class UserController {
         return cs.login(dtoLogin);
     }
 
+    /**
+     * Endpoint per effettuare il login di un utente.
+     *<p>
+     * Questo metodo riceve una richiesta di login contenente l'email e la password dell'utente,
+     * e restituisce un oggetto {@link UserLoginRespDto} contenente i dati dell'utente loggato.
+     * Se le credenziali sono errate, vengono sollevate delle eccezioni appropriate.
+     *
+     * @param dto Oggetto contenente le credenziali di login dell'utente. Non può essere null.
+     * @return Un oggetto {@link UserLoginRespDto} contenente il token di autenticazione, l'id, l'username,
+     *         il punteggio e la difficoltà dell'utente. Non può essere null.
+     * @throws NullPointerException se {@code dto} è null.
+     * @throws com.generationtycoon.controllers.exceptions.InvalidEmailException se l'email fornita non corrisponde a nessun utente registrato.
+     * @throws com.generationtycoon.controllers.exceptions.InvalidPasswordException se la password fornita non corrisponde a quella dell'utente.
+     */
     @PostMapping("/login")
     public UserLoginRespDto login(@RequestBody UserLoginReqDto dto) {
         return cs.login(dto);
     }
 
-
+    /**
+     * Endpoint per calcolare lo score di un utente in base ai dati forniti.
+     *<p>
+     * Questo metodo riceve una richiesta contenente i dati relativi a un quiz completato,
+     * come gli orari di inizio e fine, gli hp rimanenti e la difficoltà del quiz.
+     * Calcola lo score dell'utente utilizzando la logica di calcolo definita nel servizio {@link ScoreLogic}.
+     *
+     * @param dto Oggetto contenente i dati del quiz. Non può essere null.
+     * @return Un oggetto {@link UserScoreRespDto} contenente il punteggio calcolato. Non può essere null.
+     * @throws InvalidEstimateException se il calcolo dello score non è possibile (ad esempio, se i dati sono incompleti o non validi).
+     */
     @PostMapping("/score")
     public UserScoreRespDto calculateScore(@RequestBody UserScoreReqDto dto) {
         cs.getUserByToken();
